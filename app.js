6acb8cf41408b1d12b083f28eea825dc1c22822f
YaakovG <ygoldenbergyo@gmail.com>
2024-04-27 22:08:39 -0400

    did it

---
+++ app.js
@@ -0,0 +1,27 @@
+let lastName;
+var age;
+
+const firstName = "Yaakov";
+lastName = "Goldenberg";
+age = 25;
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${firstName}${lastName} is ${age} he knows ${language} from 
+b4.${language} was created in ${createdYear} it is ${isCaseSensitive} that its case sensitive. 
+${language} is not ${price} because its free to use for all developers.`);
